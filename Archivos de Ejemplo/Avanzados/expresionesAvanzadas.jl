10de51a2e277df53b7fce1e83d6565fc3a9cada1
Manuel <3057476220301@ingenieria.usac.edu.gt>
2021-08-07 10:46:41 -0600

    Se agregó expresiones avanzadas

--- Archivos de Ejemplo/Avanzados/expresionesAvanzadas.jl
+++ Archivos de Ejemplo/Avanzados/expresionesAvanzadas.jl
@@ -1,7 +1,26 @@
-#=
-    En el archivo de expresiones avanzadas se colocarán las siguientes operaciones:
-        Nativas arítmeticas como: log, log10, sin, cos, tan, sqrt
-        Operaciones con cadenas como: Concatenar, Repetir, Acceder a char o porción, length, uppercase.
-        Operador ternario
-        Utilizando distintos tipos como: entero, float, cadenas
-=#
+println("Funciones nativas aritmeticas");
+# log(base, numero)
+println(log(2, 4));     # 2.0
+println(log(9, 135));   # 2.2324867603589635
+# log10()
+println(log10(2000));   # 3.3010299956639813
+println(log10(512));    # 2.709269960975831
+# trigonometricas
+println(sin(67/360*2*3.14));    # 0.9202730580752193
+println(cos(67/360*2*3.14));    # 0.39127675446016985
+println(tan(67/360*2*3.14));    # 2.351974778938468
+# sqrt
+println(sqrt(2^4));     # 4.0
+println(sqrt(1258));    # 35.4682957019364
+
+println("Operaciones con cadenas");
+println("para" * "caidismo");   # paracaidismo
+println("Holaaa"^5);    # HolaaaHolaaaHolaaaHolaaaHolaaa
+println("Hola Mundo!"[begin:5] * "Auxiliar"[1] * "Auxiliar"[2:end]);    # Hola Auxiliar
+println(length("Esto no sé cuanto mide"));  # 22
+println(uppercase("mayuscula"));    # MAYUSCULA
+println(lowercase("MINUSCULA"));    # minuscula
+
+println("Operador ternario");
+animal = "Tortuga"
+println(animal == "Perro" ? 20 : "No"); # No
